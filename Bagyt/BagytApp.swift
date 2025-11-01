97d5c42ea9a861bd6eb2cbb15d08a542aaf9525b
Zhantik <frogfather957@gmail.com>
2025-11-01 13:42:24 +0500

    Initial Commit

---
+++ Bagyt/BagytApp.swift
@@ -0,0 +1,17 @@
+//
+//  BagytApp.swift
+//  Bagyt
+//
+//  Created by Жантемир Бериков on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BagytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
